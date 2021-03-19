2b5c97d9b2f077b4d579feacdc2b4280d1a1f202
unknown <kevin.cheung.kai@gmail.com>
2021-03-19 17:16:20 -0400

    added comments

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 with open('./Input/Letters/starting_letter.txt') as letter, open('./Input/Names/invited_names.txt') as guest_names:
     contents = letter.read()
     for name in guest_names:
+        # removes the new line from each name
         revised_name = name.strip('\n')
+
+        # replaces [name] with each invited individuals name
         contents_name = contents.replace('[name]', revised_name)
-        # print(contents_name)
+
+        # creates a new letter with each individuals name
         new_letter = open(f'./Output/ReadyToSend/letter_for_{revised_name}.txt', 'w')
+
+        # updates new letter with starting letter template and updated name
         update_letter = new_letter.write(contents_name)
