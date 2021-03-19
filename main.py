a2f941731da7c28ef65dbb06dc46b7bb8134e179
unknown <kevin.cheung.kai@gmail.com>
2021-03-19 17:13:16 -0400

    replaced [name] with each individual name in invited guest list

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+with open('./Input/Letters/starting_letter.txt') as letter, open('./Input/Names/invited_names.txt') as guest_names:
+    contents = letter.read()
+    for name in guest_names:
+        revised_name = name.strip('\n')
+        contents_name = contents.replace('[name]', revised_name)
+        # print(contents_name)
+        new_letter = open(f'./Output/ReadyToSend/letter_for_{revised_name}.txt', 'w')
+        update_letter = new_letter.write(contents_name)
